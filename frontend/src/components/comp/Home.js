da968208d950ecdf3111d168c1e246038fd45a88
Aarthiraman2512 <138648451+Aarthiraman2512@users.noreply.github.com>
2023-07-08 09:23:04 +0530

    Files Upload

---
+++ frontend/src/components/comp/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+          Home Page
+          <div class="hero-content">
+      <h2>Welcome to our Web Application</h2>
+      <p>Join us today to experience a seamless user experience</p>
+    </div>
+
+          
+       </div>
+
+   
+    )
+}
+
+export default Home
